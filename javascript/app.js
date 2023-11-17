fa3eb27eb1e08b246b7e53aa1f63a66378fa2bfd
unknown <chizaanicet@gmail.com>
2023-11-17 23:40:46 +0200

    add to my repository

---
+++ javascript/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',  function(){
+    const chart = Highcharts.chart('highcharts',{
+        chart:{
+            type: 'spline',
+        },
+        title: {
+            text: ' Grades',
+        },
+        xAxis:{
+            categories:['1P','2P','1S','3P','4P','2S','TOT']
+        },
+        yAxis:{
+            title:{
+              text: ''  
+            }
+        },
+        series: [{
+            name:'Semestre',
+            data:[1,2,3,4,2,8,1],
+            color: '#bcd9e7',
+        },{
+            name:'Totat',
+            data: [6,2,5,4,5,6,1],
+            color: '#58585a'
+        }]
+    });
+});
